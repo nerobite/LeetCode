3ba846e27d0a5456461a4a58dcc9cf2b7ee425db
Nikita <davydovnikitav@gmail.com>
2025-01-31 22:52:21 +0300

    Добавил решение задач с Codewars

---
+++ Codewars/Count Odd Numbers below n.py
@@ -0,0 +1,7 @@
+"""
+Given a number n, return the number of positive odd numbers below n, EASY!
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+"""
+def odd_count(n):
+    return (n - 1) // 2 + (1 if n % 2 == 0 else 0)
