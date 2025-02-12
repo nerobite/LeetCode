69cb4f20c65522beab1aa2ea3cfa0f143b3c4bc7
Nikita <davydovnikitav@gmail.com>
2025-02-12 20:13:48 +0300

    Добавил решение задачи Is this a triangle?

---
+++ Codewars/Is this a triangle.py
@@ -0,0 +1,24 @@
+"""
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+(In this case, all triangles must have surface greater than 0 to be accepted).
+Examples:
+Input -> Output
+1,2,2 -> true
+4,2,3 -> true
+2,2,2 -> true
+1,2,3 -> false
+-5,1,3 -> false
+0,2,3 -> false
+1,2,9 -> false
+"""
+def is_triangle(a, b, c):
+    if a > 0 and b > 0 and c > 0:
+        if ((a + b) > c) and ((a + c) > b) and ((b + c) > a):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+test  = is_triangle(7, 2, 2)
+print(test)
