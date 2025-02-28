6a6ad3b02148c573a648c5500e9dc77a0fc51047
Nikita Davydov <davydovnikitav@gmail.com>
2025-02-28 14:11:49 +0300

    Добавил решение задач Product Price at a Given Date

---
+++ Medium/Product Price at a Given Date.py
@@ -0,0 +1,44 @@
+"""
+Table: Products
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| new_price     | int     |
+| change_date   | date    |
++---------------+---------+
+(product_id, change_date) is the primary key (combination of columns with unique values) of this table.
+Each row of this table indicates that the price of some product was changed to a new price at some date.
+Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
+Return the result table in any order.
+The result format is in the following example.
+Example 1:
+Input:
+Products table:
++------------+-----------+-------------+
+| product_id | new_price | change_date |
++------------+-----------+-------------+
+| 1          | 20        | 2019-08-14  |
+| 2          | 50        | 2019-08-14  |
+| 1          | 30        | 2019-08-15  |
+| 1          | 35        | 2019-08-16  |
+| 2          | 65        | 2019-08-17  |
+| 3          | 20        | 2019-08-18  |
++------------+-----------+-------------+
+Output:
++------------+-------+
+| product_id | price |
++------------+-------+
+| 2          | 50    |
+| 1          | 35    |
+| 3          | 10    |
++------------+-------+
+"""
+import pandas as pd
+
+def price_at_given_date(products: pd.DataFrame) -> pd.DataFrame:
+    res = pd.DataFrame({"product_id":products['product_id'].unique(), 'price':10})
+    df = products[products['change_date'] <= '2019-08-16']
+    df = df.loc[df.groupby('product_id')['change_date'].idxmax()]
+    res['price'] = res['product_id'].map(df.set_index('product_id')['new_price']).fillna(res['price'])
+    return res
